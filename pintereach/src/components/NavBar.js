dc97f8ebbd689a8e24b9a55fbc88b4daf8c32020
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-11-17 19:08:03 -0500

    added reducers and navbar

---
+++ pintereach/src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { PageHeader } from "antd";
+
+const NavBar = () => {
+  return (
+    <div
+      style={{ width: "30%", display: "flex", justifyContent: "space-between" }}
+    >
+      <NavLink to="/signup" className="NavBtn">
+        {" "}
+        Signup{" "}
+      </NavLink>
+      <NavLink to="/login" className="NavBtn">
+        {" "}
+        Login{" "}
+      </NavLink>
+      <NavLink to="/home" className="NavBtn">
+        {" "}
+        Your Profile{" "}
+      </NavLink>
+      <NavLink to="/login" className="NavBtn">
+        {" "}
+        Logout{" "}
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavBar;
--- pintereach/src/index.js
+++ pintereach/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// IMPORTS
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reducer from "./reducers/reducers.js";
+import thunk from "redux-thunk";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// COMPOSE
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// REDUX STORE
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+// RENDER
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
