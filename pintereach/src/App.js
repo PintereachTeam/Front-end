dbb3dac10c286bee58aab05a91bc9a3b42dbc311
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-18 19:48:59 -0500

    Added route to login form

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import {Route} from 'react-router-dom';
 
 import "./App.css";
 import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <Route path='/login' component={LoginForm}/>
       </header>
     </div>
   );
