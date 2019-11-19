2c0c606fc69d0c45e6c4c1236e466beeb6581a1e
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-19 18:16:30 -0500

    set up usState and usEffect and used axiosWithAuth to get boards

--- pintereach/src/components/Boards.js
+++ pintereach/src/components/Boards.js
@@ -1,9 +1,35 @@
-import React from 'react'; 
+import React, {useState, useEffect} from 'react'; 
+import axios from 'axios';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 
 const Boards = () => {
+    const [articles, setArticles] = useState([])
+    const [boards, setBoards] = useState({
+        id: 1,
+        board_title: 'Boredom Testing',
+        user_id: 3
+    });
+
+    useEffect(()=>{
+    axiosWithAuth().get('https://pintereach-backend.herokuapp.com/boards')
+    .then(response => {
+        console.log('hello form inside axios',response.data)
+        setBoards(response.data)
+    })
+    .catch(error => console.log(error))
+},[]);
+
     return(
-        <div>Boredom is a state of mind</div>
+        //want to return board name
+        //return a list of articles assigned to board
+        
+        <div>
+            <h1>My Boards</h1>
+            {boards.map(board => {})}
+        </div>
+        
+        
     )
 }
 
