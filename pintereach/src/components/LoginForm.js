f119d3bff6a8f9ec3a769522acb446c7ae7aa10c
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-18 21:30:03 -0500

    form in progress

--- pintereach/src/components/LoginForm.js
+++ pintereach/src/components/LoginForm.js
@@ -2,21 +2,38 @@ import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 
-const LoginForm = () => {
+const LoginForm = ({touched, errors, isSubmitting, status, values }) => {
     const [login, setLogin] = useState({
         username: '',
         password: ''
     })
 
+    useEffect(()=> {
+        status && setLogin(login =>[...login, status])
+    },[status]);
     
     
     return(
-        <Form>
-            <Field type='text' placeholder='Username' name='username' value={login.name}/>
-            <Field type='password' placeholder='password' name='password' value={login.password} />
-            <button type='submit'>Login</button>
-        </Form>
+        <div className='login_form'>
+            <Form>
+                <Field type='text' name='username' placeholder='Username'/>
+                {touched.username && errors.username && (<p>{errors.name}</p>)}
+                <Field type='password' placeholder='password' name='password'/>
+                {touched.password && errors.password && (<p>{errors.password}</p>)}
+                <button type='submit' disabled={isSubmitting}>Login</button>
+            </Form>
+        </div>
     )
 }
+
+const superLoginForm = withFormik({
+    mapPropsToValues({name,password}){
+        return {
+            name: name || '',
+            password: password || '',
+        };
+    },
+    
+})
 
 export default LoginForm;
