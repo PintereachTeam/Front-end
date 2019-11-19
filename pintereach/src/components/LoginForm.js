b08522c6043c5049dffecd5d7907441360914a95
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-18 22:42:29 -0500

    finished login form, and cleaned code up a bit

--- pintereach/src/components/LoginForm.js
+++ pintereach/src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 const LoginForm = ({touched, errors, isSubmitting, status, values }) => {
     const [login, setLogin] = useState({
@@ -16,9 +17,9 @@ const LoginForm = ({touched, errors, isSubmitting, status, values }) => {
     return(
         <div className='login_form'>
             <Form>
-                <Field type='text' name='username' placeholder='Username'/>
+                <Field type='text' name='username' placeholder='Username' value={values.username} />
                 {touched.username && errors.username && (<p>{errors.name}</p>)}
-                <Field type='password' placeholder='password' name='password'/>
+                <Field type='password' placeholder='password' name='password' value={values.password} />
                 {touched.password && errors.password && (<p>{errors.password}</p>)}
                 <button type='submit' disabled={isSubmitting}>Login</button>
             </Form>
@@ -27,13 +28,29 @@ const LoginForm = ({touched, errors, isSubmitting, status, values }) => {
 }
 
 const superLoginForm = withFormik({
-    mapPropsToValues({name,password}){
+    mapPropsToValues({username,password}){
         return {
-            name: name || '',
+            username: username || '',
             password: password || '',
         };
     },
-    
-})
 
-export default LoginForm;
+    validationSchema:Yup.object().shape({
+        username: Yup.string().required('Username is required!'),
+        password: Yup.string().required('Password is required!').min(8, 'Password must be 8 characters')
+    }),
+    handleSubmit(values, {resetForm, setErrors, setSubmitting, setStatus}){
+        axios.post('https://pintereach-backend.herokuapp.com/auth/login', values)
+        .then(response => {
+            console.log(response);
+            resetForm();
+            setSubmitting(false);
+            setStatus(response.data);
+        })
+        .catch(error => console.log(error));
+        setSubmitting(false);
+    }
+    
+})(LoginForm)
+
+export default superLoginForm;
