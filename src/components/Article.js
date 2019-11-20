2a406cf4b4904ffa36fc79b2aed8803d90446e03
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-20 14:21:48 -0500

    Reworked to display article list

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Form, Input, Button } from "antd";
+import ArticlesList from './ArticlesList';
 
 export default function Article(props) {
   const { hideModal, addArticle, modalDisplay } = props;
@@ -29,53 +29,6 @@ export default function Article(props) {
   };
 
   return (
-    <>
-      <Modal
-        title="Add New Article"
-        visible={modalDisplay.visible}
-        onCancel={hideModal}
-        footer={[
-          <Button
-            form="articleForm"
-            key="submit"
-            htmlType="submit"
-            onClick={clickAddArticle}
-          >
-            Add Article
-          </Button>
-        ]}
-      >
-        <Form>
-          <label htmlFor="URL">URL</label>
-          <Input
-            type="text"
-            name="link"
-            value={article.link}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="title">Title</label>
-          <Input
-            type="text"
-            name="title"
-            value={article.title}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="summary">Summary</label>
-          <Input
-            type="text"
-            name="summary"
-            value={article.summary}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="category">Tags</label>
-          <Input
-            type="text"
-            name="category"
-            value={article.category}
-            onChange={handleInputChange}
-          />
-        </Form>
-      </Modal>
-    </>
+    <ArticlesList/>
   );
 }
