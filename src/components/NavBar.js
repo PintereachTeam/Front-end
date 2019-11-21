8f04513d0d8a7507707de1805f77366995dee9be
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-11-21 16:38:22 -0500

    Added welcome header to login form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,13 @@ import logo from '../img/icon_white.png'
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className="deep-purple darken-3">
     <div className='nav-wrapper #7804eb lighten-2' style={{display:'flex', justifyContent: 'space-between', width: '100%'}}>
      <div>
       <img className="brand-logo left" src={logo} alt='pinterech_logo' style={{width:50 , height: 50}} />
       </div>
        <div style={{display: 'flex', justifyContent: 'space-between', width: 500}}>
+         
         <NavLink to="/signup" >
           Signup
         </NavLink>
